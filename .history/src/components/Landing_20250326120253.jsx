5a174dfd2c169686f4d60812b406cc5fbc746397
rkCodeX <rimshashoukat271@gmail.com>
2025-03-26 12:40:16 +0500

    landing page styling added

---
+++ .history/src/components/Landing_20250326120253.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import landingBg from "../assets/landing-bg.jpg";
+
+function Landing(){
+    return(
+        <>
+           <div className="relative w-full h-screen">
+            {/* Background Image */}
+           <img className="absolute w-[100vw] h-[100vh]" src={landingBg} alt="landing Image"/> 
+           {/* Content */}
+           <div className="relative flex flex-col gap-[3rem] py-[2rem] w-[100vw] h-[100vh]">
+                <nav className="flex flex-row items-center justify-between px-[3rem] w-[100vw] h-[2rem]">
+                         <button className="font-extrabold font-serif text-lg">shoppey</button>
+                    <div className="flex font-bold font-serif flex-row items-center justify-evenly gap-[2rem]">
+                        <button className="cursor-pointer hover:border-b-2 border-b-black">New</button>
+                        <button className="cursor-pointer hover:border-b-2 border-b-black">Men</button>
+                        <button className="cursor-pointer hover:border-b-2 border-b-black">Women</button>
+                        <button className="cursor-pointer hover:border-b-2 border-b-black">Collection</button>
+                        <button className="cursor-pointer hover:border-b-2 border-b-black">Trends</button>
+                    </div>
+                    <button className="font-bold font-serif cursor-pointer border-2 border-black px-[0.5rem] py-[0.2rem] hover:text-[#b48068] hover:bg-black">LOGIN</button>
+                </nav>
+                <main className="flex flex-row items-center justify-center gap-[5rem] w-[100%] h-[100%] border-2 border-red-900">
+                    <section className="w-[50%] h-[100%] flex flex-col items-center justify-center gap-[1rem] border-2 border-black">
+                        h1*2
+                    </section>
+                    <img className="w-[50%] h-[100%] border-2 border-amber-700" src="" alt="side-img" />
+                </main>
+           </div>
+           </div>
+        </>
+    )
+}
+
+export default Landing;
