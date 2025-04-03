d712b699bfd5fea4433199a787c1f5407c16ebba
rkCodeX <rimshashoukat271@gmail.com>
2025-04-03 12:42:45 +0500

    drop shadow added to last section in collection

--- .history/src/components/newCollection_20250403124015.jsx
+++ .history/src/components/newCollection_20250403124015.jsx
@@ -11,7 +11,7 @@ function newCollection(){
                         <h1 className="text-3xl font-bold select-none text-nowrap">NEW COLLECTION <br/> 2025</h1>
                         <p className="text-justify select-none">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus dicta suscipit ducimus incidunt vel. Consectetur, magnam velit eligendi delectus tempore dicta iure amet deserunt. Mollitia neque similique corrupti laborum tenetur?</p>
                     </div>
-                    <section className="absolute bottom-0 right-0 drop-shadow-lg w-[70%] max-[710px]:w-[90%] bg-gray-200 flex flex-row select-none p-[0.5rem] pb-[1rem]">
+                    <section className="absolute bottom-0 right-0 shadow- w-[70%] max-[710px]:w-[90%] bg-gray-200 flex flex-row select-none p-[0.5rem] pb-[1rem]">
                         <div className="border-r-2 border-r-gray-600 w-[33%] text-center">
                             <h1 className="text-[2rem] max-[710px]:text-[1.3rem] font-bold">2025</h1>
                             <p className="text-semibold leading-[0.5]">Year</p>
