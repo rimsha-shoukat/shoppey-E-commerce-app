839aa84daccde4270f33b8d85dde1eccb097ab35
rkCodeX <rimshashoukat271@gmail.com>
2025-06-13 21:49:27 +0500

    responsiveness and styling added to buttons and heading

---
+++ .history/src/components/Product_20250613211229.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { FaSliders } from "react-icons/fa6";
+import demo from "../assets/seller13.png";
+
+function Product(){
+    return(
+        <>
+         <div className="w-[100%] h-auto p-[4rem] flex flex-col items-center justify-center ">
+            <h1>Our Product</h1>
+            {/* categories button section */}
+            <section>
+                <button>NEW</button>
+                <button>WOMEN</button>
+                <button>MEN</button>
+                <button>Trends</button>
+                <button><FaSliders /> Filter</button>
+            </section>
+            {/* Product section */}
+            <section>
+                <img src={demo} alt="" />
+                <img src={demo} alt="" />
+                <img src={demo} alt="" />
+                <img src={demo} alt="" />
+                <img src={demo} alt="" />
+                <img src={demo} alt="" />
+                <img src={demo} alt="" />
+                <img src={demo} alt="" />
+                <img src={demo} alt="" />
+                <img src={demo} alt="" />
+            </section>
+         </div>
+        </>
+    )
+}
+
+export default Product;
--- .history/src/components/Product_20250613214157.jsx
+++ .history/src/components/Product_20250613214157.jsx
@@ -8,14 +8,14 @@ function Product(){
          <div className="w-[100%] h-auto p-[4rem] max-[430px]:p-[1rem] flex flex-col items-center justify-center font-serif select-none">
             <h1 className="font-bold text-[clamp(2rem,5vw,5rem)]">Our Product</h1>
             {/* categories button section */}
-            <section className="flex flex-row items-center justify-center gap-4 max-[310px]:gap-2 leading-4 w-[100%] h-[2rem] max-[430px]:text-sm">
+            <section className="flex flex-row items-center justify-center gap-4 leading-4 w-[100%] h-[2rem] max-[430px]:">
                 <button className="cursor-pointer hover:border-b-2">NEW</button>
                 <button className="cursor-pointer hover:border-b-2">WOMEN</button>
                 <button className="cursor-pointer hover:border-b-2">MEN</button>
-                <button className="cursor-pointer hover:border-b-2">TREND</button>
+                <button className="cursor-pointer hover:border-b-2">TRENDS</button>
                 <div className="flex flex-row items-center justify-center gap-2 cursor-pointer group">
                     <FaSliders />
-                    <button className="group-hover:border-b-2 max-[350px]:hidden">Filter</button>
+                    <button className="group-hover:border-b-2">Filter</button>
                 </div>
             </section>
             {/* Product section */}
