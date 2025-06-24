81e5270d69393ca0278391db99bfb1c85abbbf4f
rkCodeX <rimshashoukat271@gmail.com>
2025-06-24 21:28:43 +0500

    responsiveness added to social component

--- .history/src/components/Social_20250624210457.jsx
+++ .history/src/components/Social_20250624210457.jsx
@@ -8,16 +8,16 @@ import img5 from "../assets/seller15.png";
 function Social(){
     return(
         <> 
-            <section className="flex flex-col items-center justify-center w-[100%] my-[1rem] h-auto font-serif relative select-none overflow-hidden">
-                <div className="flex flex-row items-center justify-center gap-0 flex-wrap">
+            <section className="flex flex-col items-center justify-center w-[100%] h-auto font-serif p-6 relative select-none">
+                <div className="flex flex-row items-center justify-center gap-0">
                     <img className="w-[15.5rem] h-[12rem]" src={img1} alt="" />
                     <img className="w-[15.5rem] h-[12rem]" src={img2} alt="" />
                     <img className="w-[15.5rem] h-[12rem]" src={img3} alt="" />
                     <img className="w-[15.5rem] h-[12rem]" src={img4} alt="" />
                     <img className="w-[15.5rem] h-[12rem]" src={img5} alt="" />
                 </div>
-            <h1 className="absolute px-[1.45rem] py-[0.65rem] bg-[#eee2ca] border text-[1rem] font-semibold border-[#c8967d] bottom-[5.7rem]">Follow us on social media</h1>
-                <div className="flex flex-row items-center justify-evenly w-[100%] h-auto mt-[2rem] max-[440px]:text-xs">
+            <h1 className="absolute px-[2rem] py-[rem] bg-[#eee2ca] border text-[1.2rem] font-semibold border-[#c8967d] bottom-[8rem]">Follow us on social media</h1>
+                <div className="flex flex-row items-center justify-evenly w-[100%] h-auto mt-[2rem]">
                     <h1 className="cursor-pointer font-semibold">NEW</h1>
                     <h1 className="cursor-pointer font-semibold">WOMEN</h1>
                     <h1 className="cursor-pointer font-semibold">MEN</h1>
--- .history/src/components/Social_20250624211534.jsx
+++ .history/src/components/Social_20250624211534.jsx
@@ -8,7 +8,7 @@ import img5 from "../assets/seller15.png";
 function Social(){
     return(
         <> 
-            <section className="flex flex-col items-center justify-center w-[100%] my-[1rem] h-auto font-serif relative select-none overflow-hidden">
+            <section className="flex flex-col items-center justify-center w-[100%] h-auto font-serif p-6 relative select-none">
                 <div className="flex flex-row items-center justify-center gap-0 flex-wrap">
                     <img className="w-[15.5rem] h-[12rem]" src={img1} alt="" />
                     <img className="w-[15.5rem] h-[12rem]" src={img2} alt="" />
@@ -16,8 +16,8 @@ function Social(){
                     <img className="w-[15.5rem] h-[12rem]" src={img4} alt="" />
                     <img className="w-[15.5rem] h-[12rem]" src={img5} alt="" />
                 </div>
-            <h1 className="absolute px-[1.45rem] py-[0.65rem] bg-[#eee2ca] border text-[1rem] font-semibold border-[#c8967d] bottom-[5.7rem]">Follow us on social media</h1>
-                <div className="flex flex-row items-center justify-evenly w-[100%] h-auto mt-[2rem] max-[440px]:text-xs">
+            <h1 className="absolute px-[1.5rem] py-[0.6rem] bg-[#eee2ca] border text-[1rem] font-semibold border-[#c8967d] bottom-[8rem]">Follow us on social media</h1>
+                <div className="flex flex-row items-center justify-evenly w-[100%] h-auto mt-[2rem] max-[440px]:text-sm">
                     <h1 className="cursor-pointer font-semibold">NEW</h1>
                     <h1 className="cursor-pointer font-semibold">WOMEN</h1>
                     <h1 className="cursor-pointer font-semibold">MEN</h1>
