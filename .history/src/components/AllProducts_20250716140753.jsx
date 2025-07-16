49a54267ac8df98bbb021e666be8d77079eeef99
rkCodeX <rimshashoukat271@gmail.com>
2025-07-16 15:40:50 +0500

    Products list added

---
+++ .history/src/components/AllProducts_20250716140753.jsx
@@ -0,0 +1,209 @@
+import React from 'react';
+import { FaRegUser } from "react-icons/fa";
+import { FaCartShopping } from "react-icons/fa6";
+import { BiSearch } from "react-icons/bi";
+import { Link } from 'react-router-dom';
+import { FaRegHeart } from "react-icons/fa6";
+
+import img1 from "../assets/w1.png";
+import img2 from "../assets/w2.png";
+import img3 from "../assets/w3.png";
+import img4 from "../assets/w4.png";
+import img5 from "../assets/w5.png";
+import img6 from "../assets/w6.png";
+import img7 from "../assets/w7.png";
+import img8 from "../assets/w8.png";
+import img9 from "../assets/w9.png";
+import img10 from "../assets/w10.png";
+import img11 from "../assets/w11.png";
+import img12 from "../assets/w12.png";
+import img13 from "../assets/w13.png";
+import img14 from "../assets/w14.png";
+import img15 from "../assets/w15.png";
+import img16 from "../assets/w16.png";
+import img17 from "../assets/w17.png";
+import img18 from "../assets/w18.png";
+import img19 from "../assets/w19.png";
+import img20 from "../assets/w20.png";
+import img21 from "../assets/w21.png";
+import img22 from "../assets/w22.png";
+import img23 from "../assets/w23.png";
+import img24 from "../assets/w24.png";
+import img25 from "../assets/w25.png";
+import img26 from "../assets/w26.png";
+import img27 from "../assets/w27.png";
+import img28 from "../assets/w28.png";
+import img29 from "../assets/w29.png";
+import img30 from "../assets/w30.png";
+import img31 from "../assets/w31.png";
+import img32 from "../assets/w32.png";
+import img33 from "../assets/w33.png";
+import img34 from "../assets/w34.png";
+import img35 from "../assets/w35.png";
+import img36 from "../assets/w36.png";
+import img37 from "../assets/w37.png";
+import img38 from "../assets/w38.png";
+import img39 from "../assets/w39.png";
+import img40 from "../assets/w40.png";
+import img41 from "../assets/w41.png";
+import img42 from "../assets/w42.png";
+import img43 from "../assets/w43.png";
+import img44 from "../assets/w44.png";
+import img45 from "../assets/w45.png";
+import img46 from "../assets/w46.png";
+import img47 from "../assets/w47.png";
+import img48 from "../assets/w48.png";
+import img49 from "../assets/w49.png";
+import img50 from "../assets/w50.png";
+import img51 from "../assets/m1.png";
+import img52 from "../assets/m2.png";
+import img53 from "../assets/m3.png";
+import img54 from "../assets/m4.png";
+import img55 from "../assets/m5.png";
+import img56 from "../assets/m6.png";
+import img57 from "../assets/m7.png";
+import img58 from "../assets/m8.png";
+import img59 from "../assets/m9.png";
+import img60 from "../assets/m10.png";
+import img61 from "../assets/m11.png";
+import img62 from "../assets/m12.png";
+import img63 from "../assets/m13.png";
+import img64 from "../assets/m14.png";
+import img65 from "../assets/m15.png";
+import img66 from "../assets/m16.png";
+import img67 from "../assets/m17.png";
+import img68 from "../assets/m18.png";
+import img69 from "../assets/m19.png";
+import img70 from "../assets/m20.png";
+import img71 from "../assets/m21.png";
+import img72 from "../assets/m22.png";
+import img73 from "../assets/m23.png";
+import img74 from "../assets/m24.png";
+import img75 from "../assets/m25.png";
+import img76 from "../assets/m26.png";
+import img77 from "../assets/m27.png";
+import img78 from "../assets/m28.png";
+import img79 from "../assets/m29.png";
+import img80 from "../assets/m30.png";
+import img81 from "../assets/m31.png";
+import img82 from "../assets/m32.png";
+import img83 from "../assets/m33.png";
+import img84 from "../assets/m34.png";
+import img85 from "../assets/m35.png";
+import img86 from "../assets/m36.png";
+import img87 from "../assets/m37.png";
+import img88 from "../assets/m38.png";
+import img89 from "../assets/m39.png";
+import img90 from "../assets/m40.png";
+import img91 from "../assets/m41.png";
+import img92 from "../assets/m42.png";
+import img93 from "../assets/m43.png";
+import img94 from "../assets/m44.png";
+import img95 from "../assets/m45.png";
+import img96 from "../assets/m46.png";
+import img97 from "../assets/m47.png";
+import img98 from "../assets/m48.png";
+import img99 from "../assets/m49.png";
+import img100 from "../assets/m50.png";
+import img101 from "../assets/k1.png";
+import img102 from "../assets/k2.png";
+import img103 from "../assets/k3.png";
+import img104 from "../assets/k4.png";
+import img105 from "../assets/k5.png";
+import img106 from "../assets/k6.png";
+import img107 from "../assets/k7.png";
+import img108 from "../assets/k8.png";
+import img109 from "../assets/k9.png";
+import img110 from "../assets/k10.png";
+import img111 from "../assets/k11.png";
+import img112 from "../assets/k12.png";
+import img113 from "../assets/k13.png";
+import img114 from "../assets/k14.png";
+import img115 from "../assets/k15.png";
+import img116 from "../assets/k16.png";
+import img117 from "../assets/k17.png";
+import img118 from "../assets/k18.png";
+import img119 from "../assets/k19.png";
+import img120 from "../assets/k20.png";
+import img121 from "../assets/k21.png";
+import img122 from "../assets/k22.png";
+import img123 from "../assets/k23.png";
+import img124 from "../assets/k24.png";
+import img125 from "../assets/k25.png";
+import img126 from "../assets/k26.png";
+import img127 from "../assets/k27.png";
+import img128 from "../assets/k28.png";
+import img129 from "../assets/k29.png";
+import img130 from "../assets/k30.png";
+import img131 from "../assets/k31.png";
+import img132 from "../assets/k32.png";
+import img133 from "../assets/k33.png";
+import img134 from "../assets/k34.png";
+import img135 from "../assets/k35.png";
+import img136 from "../assets/k36.png";
+import img137 from "../assets/k37.png";
+import img138 from "../assets/k38.png";
+import img139 from "../assets/k39.png";
+import img140 from "../assets/k40.png";
+import img141 from "../assets/k41.png";
+import img142 from "../assets/k42.png";
+import img143 from "../assets/k43.png";
+import img144 from "../assets/k44.png";
+import img145 from "../assets/k45.png";
+import img146 from "../assets/k46.png";
+import img147 from "../assets/k47.png";
+import img148 from "../assets/k48.png";
+import img149 from "../assets/k49.png";
+import img150 from "../assets/k50.png";
+
+const Products = [
+  {
+    
+    name : "Jeans with pockets",
+    price : 450.34,
+    size : "M",
+    src : img1,
+    category : "kids",
+    
+
+  }
+]
+
+function AllProducts() {
+  return (
+    <>
+      <div className="w-[100%] h-auto font-serif">
+
+        <section className="flex flex-row items-center py-2 px-6 justify-between w-[100%] h-auto">
+            <Link to="/">
+              <button className="font-extrabold font-serif text-xl cursor-pointer hover:text-gray-500">shoppey</button>
+            </Link>
+            <div className="flex flex-row items-center justify-center gap-6 text-xl">
+              <Link to="/Save">
+                <FaRegHeart className="cursor-pointer hover:text-gray-500" />
+              </Link>
+              <Link to="/Cart">
+                <FaCartShopping className="cursor-pointer hover:text-gray-500" />
+              </Link>
+                <FaRegUser className="cursor-pointer hover:text-gray-500" />
+
+            </div>
+        </section>
+
+        <section className="flex flex-col items-center justify-center w-[100%] p-6 h-auto bg-linear-to-l from-[#dd957a] to-[#eee2ca] gap-2">
+              <h1 className="font-bold text-2xl">Explore All Products</h1>
+                <div className=" flex flex-row items-center justify-center w-[100%] gap-2">
+                    <input type="text" placeholder="Search" className="w-[50%] max-[550px]:w-[90%] h-[2.5rem] px-4 rounded-full  border-none bg-white/50 text-black placeholder:text-gray-700 focus:outline-none focus:ring-2 focus:ring-[#b48068]" />
+                    <BiSearch className="font-bold cursor-pointer text-2xl hover:text-[#b48068]"/>
+                </div>
+        </section>
+
+        <section className="columns-5 mt-[1rem] w-[100%] h-auto p-4 max-[900px]:columns-4 max-[660px]:columns-3 max-[500px]:columns-2">
+          <img className="w-full rounded-[1rem] mb-6 h-auto shadow-[0.1px_0.1px_0.1rem_#dd957a]" src={img1} />
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default AllProducts;
