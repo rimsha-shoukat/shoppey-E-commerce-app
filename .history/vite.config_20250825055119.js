7f595847133be77530268f30ed8b7a67dea1eede
rimsha-shoukat <rimsha.devs@gmail.com>
2025-08-25 05:54:03 +0500

    base added to vite.config.js

--- .history/vite.config_20250825055119.js
+++ .history/vite.config_20250825055119.js
@@ -4,5 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss() ],
-  base: '/shoppey-E-commerce-app/',
+  base: '/shoppey-E-commerc/',
 })
