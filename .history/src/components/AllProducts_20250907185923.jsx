704e56ded75a84c9f68594621ea8e0d3f2308de9
rimsha-shoukat <rimsha.devs@gmail.com>
2025-09-07 21:10:57 +0500

    logOn and logout interactive

---
+++ .history/src/components/AllProducts_20250907185923.jsx
@@ -0,0 +1,91 @@
+import React , { useState , useEffect, useRef } from "react";
+import { FaRegUser } from "react-icons/fa";
+import { FaCartShopping } from "react-icons/fa6";
+import { BiSearch } from "react-icons/bi";
+import { Link } from 'react-router-dom';
+import { FaRegHeart } from "react-icons/fa6";
+import { useParams } from 'react-router-dom';
+
+function AllProducts({Products}) {
+  const { param } = useParams();
+  const [ filterProducts , setFilterProducts ] = useState(Products);
+  const searchRef = useRef(null);
+  const user = JSON.parse(localStorage.getItem('currentUser')) || [];
+
+  function handleSearchItems() {
+    if (searchRef.current.value !== "") {
+      const searchProducts = Products.filter((product) =>
+       product.title.toLowerCase().includes(searchRef.current.value.toLowerCase()));
+      if(searchProducts.length > 0){
+        setFilterProducts(searchProducts);
+      }else{
+        alert("no Item Found");
+      }
+    }
+  }
+ 
+  useEffect( () => {
+    let Items = [...Products];
+
+    if(param){
+      if(['beauty' , 'fragrances' , 'furniture',  'groceries'].includes(param)){
+        Items = Items.filter(item => item.category === param);
+      } else if(['rating', 'discountPercentage'].includes(param)){
+        if(param === "rating"){
+          Items = Items.filter(item => item.rating >= 3);
+        } else if(param === "discountPercentage"){
+          Items = Items.filter(item => item.discountPercentage > 10);
+        } 
+      }
+    }
+    
+    setFilterProducts(Items);
+
+  }, [param]);
+
+  return (
+    <>
+      <div className="w-[100%] h-auto font-serif">
+
+        <section className="flex flex-row items-center py-2 px-6 justify-between w-[100%] h-auto">
+            <Link to="/">
+              <button className="font-extrabold font-serif text-xl cursor-pointer hover:text-gray-500">shoppey</button>
+            </Link>
+            <div className="flex flex-row items-center justify-center gap-6 text-xl">
+              <Link to="/Save">
+                <FaRegHeart className="cursor-pointer hover:text-gray-500" />
+              </Link>
+              <Link to="/Cart">
+                <FaCartShopping className="cursor-pointer hover:text-gray-500" />
+              </Link>
+              {
+                localStorage.getItem('account')
+              }
+              <Link to="/SignIU">
+                <FaRegUser className="cursor-pointer hover:text-gray-500" />
+              </Link>
+            </div>
+        </section>
+
+        <section className="flex flex-col items-center justify-center w-[100%] p-6 h-auto bg-linear-to-l from-[#dd957a] to-[#eee2ca] gap-2">
+              <h1 className="font-bold text-2xl">Explore All Products</h1>
+                <div className=" flex flex-row items-center justify-center w-[100%] gap-2">
+                    <input type="text" ref={searchRef}
+                    placeholder="Search" className="w-[50%] max-[550px]:w-[90%] h-[2.5rem] px-4 rounded-full  border-none bg-white/50 text-black placeholder:text-gray-700 focus:outline-none focus:ring-2 focus:ring-[#b48068]" />
+                    <BiSearch onClick={() => handleSearchItems()} className="font-bold cursor-pointer text-2xl hover:text-[#b48068]"/>
+                </div>
+        </section>
+
+        <section className="columns-5 mt-[1rem] w-[100%] h-auto p-4 max-[900px]:columns-4 max-[660px]:columns-3 max-[500px]:columns-2">
+        { filterProducts.map((item) => (
+            <Link key={item.id} to={`/ProductDetail/${item.id}`}>
+              <img key={item.id} loading="lazy"  className="w-full rounded-[1rem] mb-6 h-auto shadow-[0.1px_0.1px_0.1rem_#dd957a]" src={item.thumbnail} alt="products" />
+            </Link>
+          ))}
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default AllProducts;
---
+++ .history/src/components/Cart_20250907182956.jsx
@@ -0,0 +1,145 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { FaRegUser } from "react-icons/fa";
+import { FiMinus } from "react-icons/fi";
+import { FaPlus } from "react-icons/fa6";
+import { FaRegHeart } from "react-icons/fa6";
+import { FaArrowRight } from "react-icons/fa6";
+import { RxCross2 } from "react-icons/rx";
+
+const Carts = ({cartItemsList, setCartItemsList}) => {
+
+    useEffect(() => {
+        localStorage.setItem('CartItems', JSON.stringify(cartItemsList));
+    }, [cartItemsList]);
+
+    const remove = (id) =>{
+        const itemIndex = cartItemsList.findIndex(product => product.id === id);
+        if (itemIndex !== -1) {
+            const item = cartItemsList[itemIndex];
+            const updatedItems = [...cartItemsList];
+            updatedItems.splice(itemIndex, 1);
+            setCartItemsList(updatedItems);
+            alert(`Removed ${item.name} from cart`);
+        }
+    }
+
+    if(cartItemsList.length === 0){
+        return (
+            <section className="w-[100%] h-auto flex flex-col items-center justify-center">
+                <h1 className="text-2xl font-bold">Nothing In Cart</h1>
+            </section>
+        )
+    }
+
+    function setItemQuantity(id, quantity) {
+        const itemIndex = cartItemsList.findIndex(product => product.id === id);
+        if (itemIndex !== -1) {
+            const updatedItems = [...cartItemsList];
+            updatedItems[itemIndex].quantity = quantity;
+            setCartItemsList(updatedItems);
+        }
+    }
+
+    return (
+        <section className="w-[100%] h-auto flex flex-col items-center justify-center">
+           {cartItemsList.map((item) => (
+            <div key={item.id} className="w-[100%] flex flex-row items-center justify-center py-6 max-[350px]:py-2 gap-4 border-t-2 border-t-gray-400">
+                    <Link key={item.id} to={`/ProductDetail/${item.id}`}>
+                        <img loading="lazy" className="w-[6rem] h-[6rem] rounded-sm" src={item.thumbnail} alt="cartItem" />
+                    </Link>
+                        <div className="flex flex-row items-start justify-between gap-2 max-[350px]:gap-0 max-[500px]:flex-col">
+                            <div className="flex flex-col items-start justify-start gap-6 max-[750px]:gap-2 max-[500px]:gap-0">
+                                <h1 className="font-bold text-[1.2rem] leading-5 max-[350px]:text-[0.95rem] w-[20rem] max-[760px]:w-[15rem] max-[600px]:w-[10rem] max-[500px]:w-[15rem] max-[370px]:w-[11rem] max-[760px]:text-[1rem]">{item.title}</h1>
+                                <div className="flex flex-row gap-4">
+                                <p className="text-sm max-[350px]:text-xs">Size: {item.size}</p>
+                                <p className="text-sm max-[350px]:text-xs">${item.price}</p>
+                                </div>
+                            </div>
+                            <div className="flex flex-row items-center justify-center gap-[1rem] max-[350px]:gap-[0.75rem]">
+                                <h1 className="font-bold max-[350px]:font-semibold max-[350px]:text-sm">${(item.quantity * item.price).toFixed(2)}</h1>
+                                <div className="flex flex-row item-center justify-center gap-[1rem] max-[500px]:gap-[0.5rem] border-y-2 border-white p-2 max-[350px]:p-0 max-[350px]:border-none leading-4">
+                                <FiMinus onClick={item.quantity >= 2 ? () => setItemQuantity(item.id, item.quantity - 1) : null} className="cursor-pointer hover:text-gray-700 max-[350px]:text-sm"/>
+                                    <h1 className="text-[1.2rem] max-[350px]:text-md">{item.quantity}</h1>
+                                <FaPlus onClick={item.quantity < 10 ? () => setItemQuantity(item.id, item.quantity + 1) : null} className="cursor-pointer hover:text-gray-700 max-[350px]:text-sm"/>
+                                </div>
+                                <RxCross2 onClick={() => remove(item.id)} className="cursor-pointer hover:text-red-800" />                        
+                            </div>
+                        </div>   
+                    </div>
+           ))}
+        </section>
+    )
+}
+
+function Cart(){
+    const [cartItemsList, setCartItemsList] = useState(JSON.parse(localStorage.getItem('CartItems')) || []);
+    const [discount, setDiscount] = useState(cartItemsList.length * 7);
+    const [delivery, setDelivery] = useState(cartItemsList.length * 4);
+    let total = 0;
+    useEffect(() => {
+        if(cartItemsList.length === 0){
+            setDiscount(0);
+            setDelivery(0);
+            setBill(0);
+        }else{
+            for(let i = 0; i < cartItemsList.length; i++){
+            cartItemsList.forEach((item) => {
+                total = total + (item.price * item.quantity);
+            },)}
+            setDiscount(cartItemsList.length * 7);
+            setDelivery(cartItemsList.length * 4);
+            setBill((total + delivery) - discount);
+        }
+        
+    }, [cartItemsList]);
+
+    const [bill, setBill] = useState( (total + delivery) - discount );
+
+    return(
+        <>
+            <div className="w-[100%] h-auto font-serif flex flex-col items-center justify-between select-none px-4 max-[350px]:px-2">
+                <section className="flex flex-row items-center py-2 px-2 justify-between w-[100%] h-auto">
+                    <Link to="/">
+                      <button className="font-extrabold font-serif text-xl cursor-pointer hover:text-gray-700">shoppey</button>
+                    </Link>
+                    <div className="flex flex-row items-center justify-center gap-6 text-xl">
+                        <Link to="/Save">
+                            <FaRegHeart className="cursor-pointer hover:text-gray-500" />
+                        </Link>
+                        <Link to="/SignIU">
+                            <FaRegUser className="cursor-pointer hover:text-gray-500" />
+                        </Link>
+                    </div>
+                </section>
+                <section className="mb-2 flex flex-col items-center justify-center w-[100%] p-8 h-auto bg-linear-to-l from-[#dd957a] to-[#eee2ca]">
+                    <h1 className="font-bold text-2xl">Shopping Cart</h1>
+                </section>
+                {localStorage.getItem('account"')}
+                <section className="w-[100%] h-auto flex flex-col items-center justify-center">
+                    
+                    <Carts cartItemsList={cartItemsList} setCartItemsList={setCartItemsList} />
+                    
+                    <div className="w-[100%] flex flex-row max-[500px]:flex-col items-start max-[500px]:items-center gap-2 max-[500px]:gap-4 justify-between py-6 border-t-2 border-t-gray-400">
+                        <div className="w-[100%] flex flex-col items-start justify-start gap-2">
+                            <h1 className="font-semibold">Do you have a discount?</h1>
+                            <div className="flex flex-row gap-2 items-center justify-center">
+                            <input type="text" placeholder="Coupon code" className="w-[10rem] h-[2.5rem] px-4 rounded-sm border-gray-300 bg-white/50 text-black placeholder:text-gray-700 focus:outline-none focus:ring-2 focus:ring-[#b48068]" />
+                            <FaArrowRight className="w-[2rem] h-[2rem] text-white cursor-pointer p-2 bg-black rounded-full" />
+                            </div>
+                        </div>
+                        <div className="w-[100%] flex flex-col gap-2 items-start justify-start">
+                            <h1>Delivery charges: <span className="font-bold">${delivery.toFixed(2)}</span></h1>
+                            <h1>Discount: <span className="font-bold">${discount.toFixed(2)}</span></h1>
+                            <h1 className="text-[2rem] max-[350px]:text-[1rem]">Total: <span className="font-bold">${bill.toFixed(2)}</span></h1>
+                            <button className="w-[100%] bg-black text-white rounded-sm cursor-pointer py-2">Proceed to checkout</button>
+                        </div>
+                    </div>
+                    
+                </section>
+            </div>
+        </>
+    )
+}
+
+export default Cart;
