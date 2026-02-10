db417290b8ea64775dbfb85b3ef561836e563acc
rimsha-shoukat <rimsha.devs@gmail.com>
2026-02-10 16:19:54 +0500

    profile controller added

---
+++ backend/controllers/profile.mjs
@@ -0,0 +1,53 @@
+import User from "../models/user.model.mjs";
+import mongoose from "mongoose";
+
+async function profile(req, res) {
+    try {
+
+        // convert string Id to object Id
+        const userId = new mongoose.Types.ObjectId(req.user._id);
+        // find user by id and aggregate
+        const userProfile = await User.aggregate([
+            {
+                $match: { _id: userId }
+            },
+            {
+                $lookup: {
+                    from: "orders",
+                    localField: "orders",
+                    foreignField: "_id",
+                    as: "userOrders"
+                }
+            },
+            {
+                $lookup: {
+                    from: "comments",
+                    localField: "comments",
+                    foreignField: "_id",
+                    as: "userComments"
+                }
+            },
+            {
+                $lookup: {
+                    from: "reviews",
+                    localField: "reviews",
+                    foreignField: "_id",
+                    as: "userReviews"
+                }
+            },
+            {
+                $project: {
+                    password: 0,
+                    __v: 0
+                }
+            }
+        ]);
+
+        return res.status(200).json({ userProfile });
+    } catch (error) {
+        console.log("Something went wrong" + error.message);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export default profile;
