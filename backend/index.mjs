a94aa788356493155fd8533dbf33170b5de7d797
rimsha-shoukat <rimsha.devs@gmail.com>
2026-02-10 16:22:12 +0500

    npm packages installed like cors, jwt, cookie-parsor, bcypt

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,36 +1,35 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./config/db.mjs";
+import cookieParser from "cookie-parser";
 
-// middleware auth
-// import auth from "./middleware/auth.mjs";
 // all routes
-// import profile from "./routes/profile.mjs";
-// import signin from "./routes/signin.mjs";
-// import signup from "./routes/signup.mjs";
-// import products from "./routes/products.mjs";
-// import orders from "./routes/orders.mjs";
-// import reviews from "./routes/reviews.mjs";
-// import comments from "./routes/comments.mjs";
+import account from "./routes/account.mjs";
 
+// cors options
+const corsOptions = {
+    origin: process.env.CORS_ORIGIN,
+    METHODS: "GET, POST, PUT, DELETE",
+    credentials: true,
+}
 dotenv.config();
 const app = express();
+app.use(cookieParser());
+app.use(cors(corsOptions));
 app.use(express.json());
 
-// app.use("/api/profile", auth, profile);
-// app.use("/api/auth/signin", signin);
-// app.use("/api/auth/signup", signup);
-// app.use("/api/products", auth, products);
-// app.use("/api/orders", auth, orders);
-// app.use("/api/reviews", auth, reviews);
-// app.use("/api/comments", auth, comments);
-
+// connect to database
 try {
     await connectDB();
 } catch (error) {
     console.error("Failed to connect to database:", error);
     process.exit(1);
 }
+
+// routes
+app.use("/api/account", account);
+
 
 const Port = process.env.PORT || 4000;
 app.listen(Port, () => { console.log(`Server is running on port ${Port}`); });
