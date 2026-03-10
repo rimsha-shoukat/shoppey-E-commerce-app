2bfa022609244b7c7a403632e7d378e423b2e624
rimsha-shoukat <rimsha.devs@gmail.com>
2026-03-10 12:40:26 +0500

    extra routes removed

--- src/Store/productsStore.js
+++ src/Store/productsStore.js
@@ -19,37 +19,10 @@ export const productsStore = create((set, get) => ({
             set({ error: getErrorMessage(error), loading: false });
         }
     },
-    addNewProduct: async (newProduct) => {
-        set({ loading: true, error: "", message: "" });
-        try {
-            const response = await axios.patch(`/products/addNewProduct`, { newProduct });
-            set({ message: response.data.message, loading: false });
-        } catch (error) {
-            set({ error: getErrorMessage(error), loading: false });
-        }
-    },
-    deleteProduct: async (productId) => {
-        set({ loading: true, error: "", message: "" });
-        try {
-            const response = await axios.delete(`/products/deleteProduct?productId=${productId}`);
-            set({ message: response.data.message, loading: false });
-        } catch (error) {
-            set({ error: getErrorMessage(error), loading: false });
-        }
-    },
     updateProduct: async (updatedProduct) => {
         set({ loading: true, error: "", message: "" });
         try {
             const response = await axios.patch(`/products/updateProduct`, { updatedProduct });
-            set({ message: response.data.message, loading: false });
-        } catch (error) {
-            set({ error: getErrorMessage(error), loading: false });
-        }
-    },
-    setDiscount: async (thumbnail, category, discount) => {
-        set({ loading: true, error: "", message: "" });
-        try {
-            const response = await axios.patch(`/products/setDiscount`, { thumbnail, category, discount });
             set({ message: response.data.message, loading: false });
         } catch (error) {
             set({ error: getErrorMessage(error), loading: false });
