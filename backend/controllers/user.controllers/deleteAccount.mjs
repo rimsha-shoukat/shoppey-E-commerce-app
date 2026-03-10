a92193f3c2dea785fcabc4375bb2bbec05b48207
rimsha-shoukat <rimsha.devs@gmail.com>
2026-03-10 12:38:02 +0500

    logout and delete account route added

--- backend/controllers/user.controllers/deleteAccount.mjs
+++ backend/controllers/user.controllers/deleteAccount.mjs
@@ -1,11 +1,22 @@
 import User from "../../models/user.model.mjs";
+import bcrypt from "bcryptjs";
 
 async function deleteAccount(req, res) {
     try {
         const { email, password } = req.body;
-        return res.status(200).json({ message: "Coupon added successfully" });
+        const user = await User.findOne({email});
+        if(!user){
+             return res.status(404).json({ message: "User not found" });
+        }
+
+        const isMatch = bcrypt.compare(password, user.password);
+         if (!isMatch) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+        await User.findByIdAndDelete(user._id);
+        res.clearCookie("token", { httpOnly: true });
+        return res.status(200).json({ message: "Account deleted successfully" });
     } catch (error) {
-        console.log("Something went wrong!!!", error);
         return res.status(500).json({ message: "Internal Server Error" });
     }
 }
--- backend/controllers/user.controllers/logout.mjs
+++ backend/controllers/user.controllers/logout.mjs
@@ -1,10 +1,11 @@
-import User from "../../models/user.model.mjs";
 
 async function logout(req, res) {
     try {
-        return res.status(200).json({ message: "Coupon added successfully" });
+       return res
+      .status(200)
+      .clearCookie("token", { httpOnly: true })
+      .json({ message: "User logged out successfully" });
     } catch (error) {
-        console.log("Something went wrong!!!", error);
         return res.status(500).json({ message: "Internal Server Error" });
     }
 }
